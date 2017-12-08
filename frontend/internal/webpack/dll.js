9831987304b9d29c4571898b5542bf5b770c7c1e
Nam Kolo <pokemon.namkolo@gmail.com>
2017-12-08 23:29:38 +0700

    Update webpack configuration to adapt hot reload

--- frontend/internal/webpack/dll.js
+++ frontend/internal/webpack/dll.js
@@ -3,17 +3,8 @@ var webpack = require('webpack');
 
 const buildPath = path.join(process.cwd(), 'public', 'build', 'dll');
 
-export default (env = 'developement') => ({
-  entry: {
-    vendor: ['lodash'],
-    react: ['react', 'react-dom']
-  },
-  output: {
-    path: buildPath,
-    filename: 'dll.[name].[hash].js',
-    library: '[name]_[hash]'
-  },
-  plugins: [
+export default (env = 'developement') => {
+  const plugins = [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
     }),
@@ -22,11 +13,27 @@ export default (env = 'developement') => ({
       path: path.join(process.cwd(), 'dll', '[name]-manifest.json'),
       name: '[name]_[hash]'
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ],
-  resolve: {
-    modules: ['src', 'node_modules'],
-    extensions: ['*', '.js', '.json']
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ];
+
+  if (env === 'production') {
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
   }
-});
+
+  return {
+    entry: {
+      vendor: ['lodash'],
+      react: ['react', 'react-dom']
+    },
+    output: {
+      path: buildPath,
+      filename: 'dll.[name].[hash].js',
+      library: '[name]_[hash]'
+    },
+    plugins,
+    resolve: {
+      modules: ['src', 'node_modules'],
+      extensions: ['*', '.js', '.json']
+    }
+  };
+};
