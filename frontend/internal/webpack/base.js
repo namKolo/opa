ca2a02385c32689a652405d6c1edd41450293446
Nam Kolo <pokemon.namkolo@gmail.com>
2017-12-08 18:49:21 +0700

    Add base configuration

--- frontend/internal/webpack/base.js
+++ frontend/internal/webpack/base.js
@@ -1,6 +1,28 @@
+import path from 'path';
+import webpack from 'webpack';
+
 export default {
   target: 'web',
   module: {
-    rules: [{ test: /\.js?$/, exclude: /node_modules/, loaders: ['babel-loader'] }]
+    rules: [
+      { test: /\.js?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      {
+        test: /\.ejs?$/,
+        exclude: /node_modules/,
+        loaders: ['ejs-loader']
+      }
+    ]
+  },
+  plugins: [
+    // avoiding rebuilding third lib
+    // reduce lots of incremental build time
+    new webpack.DllReferencePlugin({
+      context: '.',
+      manifest: path.join(process.cwd(), 'dll', 'vendor-manifest.json')
+    })
+  ],
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.json']
   }
 };
