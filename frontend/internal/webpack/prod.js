ca92743f49ac48716bb102e4643ba8c0c4408529
Nam Kolo <pokemon.namkolo@gmail.com>
2017-12-08 18:49:55 +0700

    Optimize production build

--- frontend/internal/webpack/prod.js
+++ frontend/internal/webpack/prod.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
@@ -7,6 +8,16 @@ import baseConfig from './base';
 
 const sourcePath = path.resolve(process.cwd(), 'src/index.js');
 const buildPath = path.resolve(process.cwd(), 'public/build');
+const dllBuildPath = path.join(process.cwd(), 'public', 'build', 'dll');
+const vendorFiles = fs
+  .readdirSync(dllBuildPath)
+  .map(file => {
+    if (file.indexOf('dll.') >= 0) {
+      return '/' + file;
+    }
+    return null;
+  })
+  .filter(Boolean);
 
 export default merge(baseConfig, {
   devtool: 'cheap-module-source-map',
@@ -24,9 +35,39 @@ export default merge(baseConfig, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
+    // uglify js
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+
     // automatically generate index.html with injected built javascript code
     new HtmlWebpackPlugin({
-      template: 'public/template.html'
+      template: 'public/template.ejs',
+      // output file name
+      filename: 'index.html',
+      inject: true,
+      vendorFiles,
+      // minify code
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
     })
   ]
 });
